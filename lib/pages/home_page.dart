816f69b4687c78020432a89d5222b68c88215e2f
Toretay <abdymvzhit@gmail.com>
2025-03-03 16:04:03 +0500

    added futurebuilder to output the data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -47,6 +47,35 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        surfaceTintColor: Colors.white,
+        title: Text("SQLite"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: getStudents(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            final List<Student> students = snapshot.data!;
+            return ListView.builder(
+              itemCount: students.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    "${students[index].id} ${students[index].name} ${students[index].age}",
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text("There are no data."));
+          }
+        },
+      ),
+    );
   }
 }
